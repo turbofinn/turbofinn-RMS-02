6c4c163fa7902bca702875e660808a15dab13d08
Shuchit Singh <shuchit.official@gmail.com>
2024-07-08 18:13:35 +0530

    categories made responsive

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import Home from './components/pages/Home/Home';
 import WelcomePage from './components/pages/welcomePage/WelcomePage';
 import VerificationPage from './components/pages/Verification/VerificationPage';
 import AddToMeal from './components/pages/addToMeal/AddToMeal';
-import Categories from './components/pages/categories/Categories'
-import Categories2 from './components/pages/categories/Categories2';
+import Categories from './components/pages/categories/Categories';
 import AddTocard from './components/pages/addToCard/AddToCard';
 import MenuMobile from "./components/pages/Menu/MenuPage";
 import MenuPage from "./components/pages/Menu/MenuPage";
 import NavBar from './components/common/NavBar/NavBar';
+
 
 function App() {
   return (
@@ -17,6 +17,7 @@ function App() {
      {/* <WelcomePage/> */}
      {/* <VerificationPage/> */}
      {/* <AddToMeal></AddToMeal> */}
+     <Categories></Categories>
      {/* <Categories></Categories> */}
      {/* <Categories2></Categories2> */}
      {/* <AddTocard/> */}
