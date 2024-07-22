32d3e68ff39c46b6230a4665a3f1511f38344446
Mohit Verma <112820522+mohitvdx@users.noreply.github.com>
2024-07-22 10:08:34 +0530

    fixes in feedback

--- src/components/pages/feedback/feedback.jsx
+++ src/components/pages/feedback/feedback.jsx
@@ -1,17 +1,13 @@
 import React, { useState } from "react";
-import {
-  Box,
-  Typography,
-  Button,
-  Rating,
-  TextField,
-  useTheme,
-} from "@mui/material";
+import { Box, Typography, Button, Rating, TextField } from "@mui/material";
 import ThankYouPage from "./thankyouForFeedback";
+import { useTheme } from "@mui/material/styles";
+import useMediaQuery from "@mui/material/useMediaQuery";
 
 const FeedbackPage = () => {
-  const [showThankYou, setShowThankYou] = useState(false);
   const theme = useTheme();
+  const matches = useMediaQuery(theme.breakpoints.up("md"));
+  const [showThankYou, setShowThankYou] = useState(false);
 
   const handleSubmit = () => {
     setShowThankYou(true);
@@ -30,53 +26,62 @@ const FeedbackPage = () => {
         alignItems: "center",
         minHeight: "100vh",
         background: `
-          linear-gradient(180deg, #0092B3 0%, rgba(83, 204, 231, 0.86) 60%),
-          linear-gradient(180deg, rgba(83, 204, 231, 0.86) 60%, rgba(217, 217, 217, 1) 40%)
+          linear-gradient(180deg, #0092B3 0%, rgba(83, 204, 231, 0.86) 70%),
+          linear-gradient(180deg, rgba(83, 204, 231, 0.86) -90%, rgba(217, 217, 217, 1) 50%)
         `,
         backgroundRepeat: "no-repeat",
-        backgroundSize: "100% 50%, 100% 50%",
+        backgroundSize: "100% 55%, 100% 45%",
         backgroundPosition: "top, bottom",
         padding: 2,
       }}
     >
-      <Typography
-        variant="h5"
-        sx={{
-          textAlign: "center",
-          marginTop: 4,
-          marginBottom: 14,
-          fontWeight: 600,
-          fontSize: "24px",
-          color: "white",
-        }}
-      >
-        Feedback
-      </Typography>
-      <Typography
-          variant="h6"
-          sx={{
-            textAlign: "center",
-            marginBottom: 3,
-            fontWeight: 600,
-            fontSize: "24px",
-            background: "linear-gradient(180deg, rgba(224, 224, 224, 1), rgba(255, 255, 255, 1))",
-            WebkitBackgroundClip: "text",
-            WebkitTextFillColor: "transparent",
-          }}
-        >
-          Rate Our Service
-        </Typography>
+      {matches ? (
+        <></>
+      ) : (
+        <>
+          {" "}
+          <Typography
+            variant="h5"
+            sx={{
+              textAlign: "center",
+              marginTop: 4,
+              marginBottom: 14,
+              fontWeight: 600,
+              fontSize: "24px",
+              color: "white",
+            }}
+          >
+            Feedback
+          </Typography>{" "}
+          <Typography
+            variant="h6"
+            sx={{
+              textAlign: "center",
+              marginBottom: 3,
+              fontWeight: 600,
+              fontSize: "24px",
+              background:
+                "linear-gradient(180deg, rgba(224, 224, 224, 1), rgba(255, 255, 255, 1))",
+              WebkitBackgroundClip: "text",
+              WebkitTextFillColor: "transparent",
+            }}
+          >
+            Rate Our Service
+          </Typography>
+        </>
+      )}
+
       <Box
         sx={{
           width: "100%",
-          maxWidth: 400,
+          maxWidth: { xs: 400, md: "557px" },
           backgroundColor: "white",
-          borderRadius: '40px',
+          borderRadius: { xs: "40px", md: "20px" },
           padding: 3,
-          boxShadow: "0px 0px 10px rgba(0,0,0,0.1)",
+          boxShadow: "0px 0px 35.6px 6px rgba(0, 0, 0, 0.25)",
+          marginTop: { xs: 0, md: "6rem" },
         }}
       >
-       
         <Box
           sx={{
             display: "flex",
@@ -85,24 +90,66 @@ const FeedbackPage = () => {
             gap: 2,
           }}
         >
+          {matches ? (
+            <>
+              <Typography
+                variant="h5"
+                sx={{
+                  textAlign: "center",
+                  marginTop: 4,
+                  marginBottom: 8,
+                  fontWeight: 600,
+                  fontSize: "3rem",
+                  color: { xs: "white", md: "black" },
+                }}
+              >
+                Feedback
+              </Typography>{" "}
+              <Typography
+                variant="h6"
+                sx={{
+                  textAlign: "center",
+                  marginBottom: 2,
+                  fontWeight: 600,
+                  fontSize: "2rem",
+                  color: "black",
+                }}
+              >
+                Rate Our Service
+              </Typography>
+            </>
+          ) : (
+            <></>
+          )}
           <Rating
             name="service-rating"
             defaultValue={4}
-            sx={{ fontSize: 52, color: "rgba(240, 149, 53, 1)", marginTop:5 }}
+            sx={{
+              fontSize: { xs: 50, md: 80 },
+              color: "rgba(240, 149, 53, 1)",
+              marginTop: 1,
+            }}
           />
-          <Typography variant="subtitle1" sx={{ fontWeight: 600, fontSize: "20px" }}>
+          <Typography
+            variant="subtitle1"
+            sx={{
+              fontWeight: 600,
+              fontSize: "20px",
+              marginTop: { xs: 0, md: "2rem" },
+            }}
+          >
             Rate My Work
           </Typography>
           <TextField
             multiline
             rows={3}
-            placeholder="Your Service Was Very Impressive"
+            placeholder="Please provide your valuable feedback"
             variant="outlined"
             fullWidth
             sx={{
-              width: "312px",
+              width: { xs: "312px", md: "430px" },
               "& .MuiOutlinedInput-root": {
-                height: "100px",
+                height: { xs: "100px", md: "130px" },
                 backgroundColor: "rgba(227, 227, 227, 0.66)",
               },
             }}
@@ -113,11 +160,12 @@ const FeedbackPage = () => {
             sx={{
               width: "287px",
               height: "44px",
-              background: "linear-gradient(180deg, rgba(9, 152, 184, 1), rgba(68, 187, 214, 1))",
-              boxShadow: "0 4px -9.5px 0 rgba(0, 0, 0, 0.25)",
+              background:
+                "linear-gradient(180deg, rgba(9, 152, 184, 1), rgba(68, 187, 214, 1))",
+              boxShadow: "0px 4px 9.5px 0px rgba(0, 0, 0, 0.25) inset",
               fontWeight: 600,
               fontSize: "20px",
-              borderRadius:'58px'
+              borderRadius: "58px",
             }}
           >
             Submit
