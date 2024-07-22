0bcdc4d09526df6f605d72f84b8d335cb99c0bfd
Shuchit Singh <shuchit.official@gmail.com>
2024-07-22 10:40:13 +0530

    updated some errors

--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,6 @@ import Verification from "./components/pages/Verification/VerificationPage.jsx";
 import Categories from "./components/pages/categories/Categories.jsx";
 import MenuPage from "./components/pages/Menu/MenuPage.jsx";
 import AddToMeal from "./components/pages/addToMeal/AddToMeal";
-import AddToMeal from "./components/pages/addToMeal/AddToMeal";
 import Profile from "./components/pages/profile/profileSetting.jsx";
 import OrderHistory from "./components/pages/orderHistory/OrderHistory.jsx";
 import AddTocard from "./components/pages/addToCard/AddToCard.jsx";
@@ -12,9 +11,9 @@ import OrderStatus from "./components/pages/orderStatus/OrderStatus.jsx";
 import BillPage from "./components/pages/billingPage/BillingPage.jsx";
 import FeedbackPage from "./components/pages/feedback/feedback.jsx";
 import LoaderSpecialToday from "./components/pages/Loader/LoaderSpecialToday.jsx";
-import LoaderBeverages from './components/pages/Loader/LoaderBeverages.jsx'
-import LoaderDineIn from './components/pages/Loader/LoaderDineIn.jsx'
-import LoaderTakeAway from './components/pages/Loader/LoaderTakeAway.jsx'
+import LoaderBeverages from './components/pages/Loader/LoaderBeverages.jsx';
+import LoaderDineIn from './components/pages/Loader/LoaderDineIn.jsx';
+import LoaderTakeAway from './components/pages/Loader/LoaderTakeAway.jsx';
 const routes = [
   {
     name: "welcome",
@@ -76,22 +75,6 @@ const routes = [
     path: "/feedbackpage",
     component: <FeedbackPage />,
   },
-];
-  {
-    name: "welcome",
-    path: "/",
-    component: <Welcome />,
-  },
-  {
-    name: "verification",
-    path: "/login",
-    component: <Verification />,
-  },
-  {
-    name: "category",
-    path: "/category",
-    component: <Categories />,
-  },
   {
     name:'specialToday',
     path: "/specialToday",
@@ -111,41 +94,6 @@ const routes = [
     name:'takeAway',
     path: "/takeAway",
     component: <LoaderTakeAway/>,
-  },
-  {
-    name: "menu",
-    path: "/menu",
-    component: <MenuPage />,
-  },
-  {
-    name: "product details",
-    path: "/productdetails",
-    component: <AddToMeal />,
-  },
-  {
-    name: "Your Meal CART",
-    path: "/yourmealcart",
-    component: <AddTocard />,
-  },
-  {
-    name: "profile",
-    path: "/profile",
-    component: <Profile />,
-  },
-  {
-    name: "Order History",
-    path: "/history",
-    component: <OrderHistory />,
-  },
-  {
-    name: "Payment Method",
-    path: "/paymentmethod",
-    component: <PaymentMethod />,
-  },
-  {
-    name: "Order Status",
-    path: "/orderstatus",
-    component: <OrderStatus />,
   },
 ];
 
