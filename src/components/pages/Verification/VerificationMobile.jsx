c57a70e5feece65c89115a0db5740540952ae428
Akki <codefeast315@gmail.com>
2024-07-28 13:49:44 +0530

    add api services

--- src/components/pages/Verification/VerificationMobile.jsx
+++ src/components/pages/Verification/VerificationMobile.jsx
@@ -87,7 +87,7 @@ const VerificationMobile = () => {
     }
 
     try {
-      api.verifyOTP( requestData ).then( (response) => {
+      api.verifyOTP(requestData).then((response) => {
 
         if (response.response.responseCode === 1001) {
 
@@ -101,7 +101,7 @@ const VerificationMobile = () => {
 
         }
       });
-    } 
+    }
     catch (error) { handleClick("Error verifying OTP", "error") }
 
     finally { setLoader(true) }
@@ -123,105 +123,163 @@ const VerificationMobile = () => {
     <React.Fragment>
 
       <Box sx={{ bgcolor: "#49C3DE", minHeight: "100vh", display: "flex", flexDirection: "column", alignItems: "center", padding: "0.125rem", fontFamily: "Poppins", position: "relative", overflowY: "auto" }}>
-        <Box sx={{ display: "flex", flexDirection: "column", alignItems: "center", minHeight: "100vh", justifyContent: "center", width: "100%", paddingBottom: "2rem" }}>
-          <Box sx={{ position: "absolute", top: 0, left: 0, right: 0, backgroundImage: `url(${backgroundfood})`, backgroundSize: "cover", backgroundPosition: "center", height: "50vh", width: "100%", opacity: 0.4 }}></Box>
 
-          <Typography variant="h4" sx={{ color: "white", marginTop: "6.87%", zIndex: 1, fontSize: "2.5rem", fontWeight: 700 }}>LOGO</Typography>
+        <Box sx={{ display: "flex", flexDirection: "column", alignItems: "center", minHeight: "100vh", justifyContent: "center", width: "100%", paddingBottom: "2rem" }}>
+
+          <Box sx={{ position: "absolute", top: 0, left: 0, right: 0, backgroundImage: `url(${backgroundfood})`, backgroundSize: "cover", backgroundPosition: "center", height: "50vh", width: "100%", opacity: 0.4 }}>
+
+          </Box>
+
+          <Typography variant="h4" sx={{ color: "white", marginTop: "6.87%", zIndex: 1, fontSize: "2.5rem", fontWeight: 700 }}>
+            LOGO
+          </Typography>
 
           <Box component="img" sx={{ height: "27.90%", width: "60.38%", zIndex: 1, marginTop: "3.37%", marginBottom: "9.0%" }} alt="" src={verification} />
-          <Box sx={{ textAlign: "center" }}>
 
-            <AnimatePresence mode="wait">
+          <Box sx={{ textAlign: "center" }} >
+
+            <AnimatePresence mode="wait" >
+
               {isOtpSent ? (
-                <motion.div {...anime(fadeInEnterOtp)} key="enter" style={{ color: "white", fontSize: "1.5rem", marginBottom: "0.1875rem", zIndex: 1, fontWeight: 600, height: "2.5rem" }}>
+
+                <motion.div {...anime(fadeInEnterOtp)} key="enter" style={{ color: "white", fontSize: "1.5rem", marginBottom: "0.1875rem", zIndex: 1, fontWeight: 600, height: "2.5rem" }} >
                   Enter Your OTP
                 </motion.div>
+
               ) : (
-                <Typography variant="h5" key="verify" component={motion.div} {...anime(fadeOutVerifyMobNum)} style={{ color: "white", fontSize: "1.5rem", marginBottom: "0.1875rem", zIndex: 1, fontWeight: 600, height: "2.5rem" }}>
+
+                <Typography variant="h5" key="verify" component={motion.div} {...anime(fadeOutVerifyMobNum)} style={{ color: "white", fontSize: "1.5rem", marginBottom: "0.1875rem", zIndex: 1, fontWeight: 600, height: "2.5rem" }} >
                   Verify Your Mobile Number
                 </Typography>
+
               )}
+
             </AnimatePresence>
 
             {isOtpSent ? (
-              <Typography component={motion.div} {...anime(easeInSubtitle)} style={{ color: "white", opacity: 0.8, fontSize: "0.875rem", fontWeight: 700, textAlign: "center", marginBottom: "10.09%", maxWidth: "350px" }}>
+
+              <Typography component={motion.div} {...anime(easeInSubtitle)} style={{ color: "white", opacity: 0.8, fontSize: "0.875rem", fontWeight: 700, textAlign: "center", marginBottom: "10.09%", maxWidth: "350px" }} >
                 "Please Let Us Know Your OTP For Verification Purposes"
               </Typography>
+
             ) : (
-              <Typography sx={{ color: "white", opacity: 0.8, fontSize: "0.875rem", fontWeight: 700, textAlign: "center", marginBottom: "10.09%", maxWidth: "350px" }}>
+              <Typography sx={{ color: "white", opacity: 0.8, fontSize: "0.875rem", fontWeight: 700, textAlign: "center", marginBottom: "10.09%", maxWidth: "350px" }} >
                 "Please Let Us Know Your Mobile Number For Verification Purposes"
               </Typography>
             )}
 
             {isOtpSent ? (
-              <Box sx={{ display: "flex", alignItems: "center", justifyItems: "center", marginLeft: 7 }}>
+
+              <Box sx={{ display: "flex", alignItems: "center", justifyItems: "center", marginLeft: 7 }} >
+
                 <OtpInput value={otp} onChange={setOtp} numInputs={4} renderInput={(props) => (
-                  <input {...props} style={{ width: "52px", height: "52px", borderRadius: "11px", border: "2.5px solid transparent", background: "linear-gradient(white, white) padding-box, linear-gradient(90deg, #515ADA 0%, #2B3074 100%) border-box", margin: "0 4px", fontSize: "24px", fontWeight: "bold", textAlign: "center", color: "#000", outline: "none" }}
-                    onKeyDown={(event) => { if (!/[0-9]/.test(event.key) && event.key !== "Backspace" && event.key !== "Delete" && event.key !== "ArrowLeft" && event.key !== "ArrowRight" && event.key !== "Tab") { event.preventDefault(); } }}
+                  <input {...props}
+                    style={{ width: "52px", height: "52px", borderRadius: "11px", border: "2.5px solid transparent", background: "linear-gradient(white, white) padding-box, linear-gradient(90deg, #515ADA 0%, #2B3074 100%) border-box", margin: "0 4px", fontSize: "24px", fontWeight: "bold", textAlign: "center", color: "#000", outline: "none" }}
+                    onKeyDown={(event) => { if (!/[0-9]/.test(event.key) && event.key !== "Backspace" && event.key !== "Delete" && event.key !== "ArrowLeft" && event.key !== "ArrowRight" && event.key !== "Tab") { event.preventDefault() } }}
                     inputMode="numeric" pattern="\d*" />
                 )} />
+
               </Box>
             ) : (
-              <Input onChange={(event) => { const value = event.target.value.replace(/\D/g, "").slice(0, 10); setMobileNumber(value); event.target.value = value; }}
-                inputProps={{ inputMode: "numeric", pattern: "[0-9]*", maxLength: 10 }} startAdornment={<InputAdornment position="start">+91</InputAdornment>} disableUnderline
-                sx={{ backgroundColor: "white", borderRadius: "0.625rem", fontSize: "1.3rem", border: "0.09375rem solid rgba(31, 104, 87, 1)", fontWeight: 600, height: "3.4375rem", width: "20.875rem", padding: "0.5rem", marginBottom: "0.5rem", "& .MuiInputBase-root": { height: "100%", alignItems: "center", padding: "0 0.5rem" }, "& .MuiInputBase-input": { padding: 0, "&::placeholder": { color: "black", opacity: 0.5 } }, "&:focus": { borderColor: "black" } }}
-                onKeyDown={(event) => { if (!/[0-9]/.test(event.key) && !["Backspace", "Delete", "ArrowLeft", "ArrowRight", "Tab"].includes(event.key)) { event.preventDefault(); } }} />
+
+              <Input onChange={(event) => { const value = event.target.value.replace(/\D/g, "").slice(0, 10); setMobileNumber(value); event.target.value = value }}
+                inputProps={{ inputMode: "numeric", pattern: "[0-9]*", maxLength: 10 }}
+                startAdornment={<InputAdornment position="start"> +91 </InputAdornment>}
+                disableUnderline
+                sx={{ backgroundColor: "white", borderRadius: "0.63rem", fontSize: "1.3rem", border: "0.094rem solid rgba(31, 104, 87, 1)", fontWeight: 600, height: "3.5rem", width: "21rem", padding: "0.5rem", marginBottom: "0.5rem", "& .MuiInputBase-root": { height: "100%", alignItems: "center", padding: "0 0.5rem" }, "& .MuiInputBase-input": { padding: 0, "&::placeholder": { color: "black", opacity: 0.5 } }, "&:focus": { borderColor: "black" } }}
+                onKeyDown={(event) => { if (!/[0-9]/.test(event.key) && !["Backspace", "Delete", "ArrowLeft", "ArrowRight", "Tab"].includes(event.key)) { event.preventDefault() } }} />
+
             )}
 
             <Box sx={{ display: "flex", alignItems: "center", marginTop: "-5px" }}>
+
               {isOtpSent ? (
                 <>
-                  <Typography sx={{ color: "rgba(243, 243, 243, 0.8)", fontSize: "13px", fontWeight: 500, margin: 0, marginLeft: 6, marginTop: 2 }}>
+
+                  <Typography sx={{ color: "rgba(243, 243, 243, 0.8)", fontSize: "13px", fontWeight: 500, margin: 0, marginLeft: 6, marginTop: 2 }} >
                     Didn't Receive The OTP?
                   </Typography>
-                  <Button onClick={sentOtpClickHandler} sx={{ color: "rgba(0, 0, 0, 0.8)", fontSize: "13px", fontWeight: 500, margin: 0, textDecoration: "underline", marginLeft: 1, marginTop: 2, padding: 0, minWidth: "auto", textTransform: "none", "&:hover": { backgroundColor: "transparent", textDecoration: "underline" } }}>
-                    Resend Code
+
+                  <Button onClick={sentOtpClickHandler} sx={{ color: "rgba(0, 0, 0, 0.8)", fontSize: "13px", fontWeight: 500, margin: 0, textDecoration: "underline", marginLeft: 1, marginTop: 2, padding: 0, minWidth: "auto", textTransform: "none", "&:hover": { backgroundColor: "transparent", textDecoration: "underline" } }} > Resend Code
                   </Button>
+
                 </>
               ) : (
                 <>
-                  <FormControlLabel control={<Checkbox size="12px" />} sx={{ marginRight: "2px", display: "flex", alignItems: "center", justifyContent: "center", marginLeft: "39px" }}></FormControlLabel>
+
+                  <FormControlLabel control={<Checkbox size="12px" />} sx={{ marginRight: "2px", display: "flex", alignItems: "center", justifyContent: "center", marginLeft: "39px" }} >
+                  </FormControlLabel>
+
                   <Typography sx={{ color: "white", fontSize: "13px", fontWeight: 500, margin: 0 }}>
                     Is This Same Number in Whatsapp
                   </Typography>
+
                 </>
               )}
+
             </Box>
 
             <AnimatePresence mode="wait">
               {isOtpSent ? (
                 <motion.div {...anime(dragUpVerifyBtn)} key="verify" style={{ width: "100%", height: "100%" }}>
-                  <Button component={Link} variant="contained" sx={{ borderRadius: "21px", backgroundColor: "rgba(9, 146, 176, 0.9)", marginTop: "8.62%", marginBottom: "8.77%", width: "12.8rem", height: "3rem", "&:focus": { bgcolor: "rgba(9, 146, 176, 0.9)" }, boxShadow: "0px 0px 9.5px 0px rgba(0, 0, 0, 0.25)" }} onClick={() => { verifyOTP(mobileNumber, otp); console.log("OTP", otp); }}>
-                    <Typography component={motion.div} {...anime(dragDownVerifyTxt)} style={{ fontSize: "0.9375rem", fontWeight: 700, textTransform: "none" }}>
-                      {!loader ? (<CircularProgress size="2rem" style={{ color: "white", margin: 'auto', display: 'flex', justifyContent: 'center' }} />) : (<>Verify</>)}
+
+                  <Button component={Link} variant="contained"
+                    sx={{ borderRadius: "21px", backgroundColor: "rgba(9, 146, 176, 0.9)", marginTop: "8.62%", marginBottom: "8.77%", width: "12.8rem", height: "3rem", "&:focus": { bgcolor: "rgba(9, 146, 176, 0.9)" }, boxShadow: "0px 0px 9.5px 0px rgba(0, 0, 0, 0.25)" }}
+                    onClick={() => {
+                      verifyOTP(mobileNumber, otp);
+                      console.log("OTP", otp)
+                    }} >
+
+                    <Typography component={motion.div} {...anime(dragDownVerifyTxt)}
+                      style={{ fontSize: "0.9375rem", fontWeight: 700, textTransform: "none" }} >
+                      {!loader ? (
+                        <CircularProgress size="2rem" style={{ color: "white", margin: 'auto', display: 'flex', justifyContent: 'center' }} />
+                      ) : (
+                        <>Verify</>
+                      )}
                     </Typography>
+
                   </Button>
+
                 </motion.div>
               ) : (
                 <motion.div {...anime(dragUpSendOtpBtn)} key="SendOtp" style={{ width: "100%", height: "100%" }}>
+
                   <Button variant="contained" sx={{ height: "3rem", borderRadius: "21px", backgroundColor: "rgba(9, 146, 176, 0.9)", marginTop: "8.62%", marginBottom: "8.77%", width: "12.8rem", "&:focus": { bgcolor: "rgba(9, 146, 176, 0.9)" }, boxShadow: "0px 0px 9.5px 0px rgba(0, 0, 0, 0.25)" }} onClick={sentOtpClickHandler}>
-                    <Typography component={motion.div} {...anime(dragDownSendOtpTxt)} style={{ fontSize: "0.9375rem", fontWeight: 700, textTransform: "none" }}>
-                      {!loader ? (<CircularProgress size="2rem" style={{ color: "white", margin: 'auto', display: 'flex', justifyContent: 'center' }} />) : (<>Send OTP</>)}
+
+                    <Typography component={motion.div} {...anime(dragDownSendOtpTxt)} style={{ fontSize: "0.9375rem", fontWeight: 700, textTransform: "none" }} >
+                      {!loader ? (
+                        <CircularProgress size="2rem"
+                          style={{ color: "white", margin: 'auto', display: 'flex', justifyContent: 'center' }} />
+                      ) : (
+                        <>Send OTP</>
+                      )}
                     </Typography>
+
                   </Button>
+
                 </motion.div>
               )}
             </AnimatePresence>
 
           </Box>
+
         </Box>
 
-        <Typography variant="body2" sx={{ position: "relative", width: "100%", textAlign: "center", color: "white", fontSize: "0.9375rem", mb: "1.0625rem", fontWeight: 700, marginTop: "auto" }}>
+        <Typography variant="body2" sx={{ position: "relative", width: "100%", textAlign: "center", color: "white", fontSize: "0.9375rem", mb: "1.0625rem", fontWeight: 700, marginTop: "auto" }} >
           A Product Of TurboFinn AI
         </Typography>
 
-        <Snackbar open={open} autoHideDuration={6000} onClose={handleClose} anchorOrigin={{ vertical: "top", horizontal: "right" }}>
-          <Alert onClose={handleClose} severity={severity} variant="filled" sx={{ width: "100%" }}>
+        <Snackbar open={open} autoHideDuration={6000} onClose={handleClose} anchorOrigin={{ vertical: "top", horizontal: "right" }} >
+
+          <Alert onClose={handleClose} severity={severity} variant="filled" sx={{ width: "100%" }} >
             {snackbarMessage}
           </Alert>
+
         </Snackbar>
 
       </Box>
+
     </React.Fragment>
   );
 };
--- src/components/pages/addToCard/AddToCard.jsx
+++ src/components/pages/addToCard/AddToCard.jsx
@@ -25,51 +25,51 @@ import ThanksModal from "../thanksPage/ThanksModal.jsx";
 import ThanksModalWeb from "../thanksPage/ThanksModalWeb.jsx";
 import api from '../../../services/apiServices.js';
 const AddTocard = () => {
-    const theme = useTheme();
-    const aspect = useMediaQuery(theme.breakpoints.up("md"));
-    const [success, setSuccess] = useState(false);
-    const [thanks, setThanks] = useState(false);
-    const [WebSuccess, setWebSuccess] = useState(false);
-    const [WebThanks, setWebThanks] = useState(false);
+  const theme = useTheme();
+  const aspect = useMediaQuery(theme.breakpoints.up("md"));
+  const [success, setSuccess] = useState(false);
+  const [thanks, setThanks] = useState(false);
+  const [WebSuccess, setWebSuccess] = useState(false);
+  const [WebThanks, setWebThanks] = useState(false);
 
-    const creatOrder = async () => {
+  const creatOrder = async () => {
 
-        const requestData = {
-            orderId: "qwdedf",
-            tableNo: "table5",
-            userId: "userId789",
-            restaurantId: "restaurantId456",
-            totalAmount: 35,
-            action: "CREATE",
-            paymentStatus: "paid",
-            orderLists: [
-                {
-                    itemId: "item1",
-                    quantity: 2,
-                    price: 10
-                },
-                {
-                    itemId: "item2",
-                    quantity: 1,
-                    price: 15
-                }
-            ],
-            orderStatus: "orderStatus",
-            customerRequest: "Please make it spicy",
-            customerFeedback: "Great service",
-            customerRating: 4.5
+    const requestData = {
+      orderId: "qwdedf",
+      tableNo: "table5",
+      userId: "userId789",
+      restaurantId: "restaurantId456",
+      totalAmount: 35,
+      action: "CREATE",
+      paymentStatus: "paid",
+      orderLists: [
+        {
+          itemId: "item1",
+          quantity: 2,
+          price: 10
+        },
+        {
+          itemId: "item2",
+          quantity: 1,
+          price: 15
         }
-
-        try {
-
-            api.createOrder( requestData ).then(( response )=>{
-                console.log( response.data );
-            });
-
-        } 
-        catch ( error ) { console.error(error) }
-        
+      ],
+      orderStatus: "orderStatus",
+      customerRequest: "Please make it spicy",
+      customerFeedback: "Great service",
+      customerRating: 4.5
     }
+
+    try {
+
+      api.createOrder(requestData).then((response) => {
+        console.log(response.data);
+      });
+
+    }
+    catch (error) { console.error(error) }
+
+  }
 
   useEffect(() => {
     if (success) {
@@ -98,7 +98,7 @@ const AddTocard = () => {
       const response = await axios.post(
         "https://kfmk2viukk.execute-api.us-east-1.amazonaws.com/dev/get-ordered-items",
         {
-          orderId: "orderI535366373",restaurantId: "308bc44a-de00-488e-b980-5ee0797e82e2",userId: "userId789",paymentStatus: "NOTPAID",
+          orderId: "orderI535366373", restaurantId: "308bc44a-de00-488e-b980-5ee0797e82e2", userId: "userId789", paymentStatus: "NOTPAID",
         },
         requestOptions
       );
@@ -129,114 +129,99 @@ const AddTocard = () => {
         <Box>
           {success && <PayConfirmationModal />}
           {thanks && <ThanksModal />}
+
           <Header />
+
           <AddToMealBox />
 
-          <Typography
-            style={{
-              color: "#525252",
-              fontSize: "1rem",
-              maxWidth: "90%",
-              marginLeft: "auto",
-              marginRight: "auto0",
-              marginTop: "1rem",
-            }}>
-            Add More Combinations
+          <Typography style={{ color: "#525252", fontSize: "1rem", maxWidth: "90%", marginLeft: "auto", marginRight: "auto", marginTop: "1rem" }} > Add More Combinations
           </Typography>
 
           <SmallMealBox />
 
-                    <Box
-                        sx={{
-                            textAlign: 'center',
-                        }}
-                    >
-                        <Button
-                            sx={{
-                                textTransform: 'none', backgroundColor: '#469DB1', color: 'white', width: '90%', padding: '0.75rem 3.75rem', fontSize: '1.3rem',
-                                '&:hover': {
-                                    backgroundColor: '#0A343D',
-                                }, borderRadius: '2rem', marginTop: '4vh', marginBottom: '1rem'
-                            }}
-                            onClick={() => {
-                                setSuccess(true);
-                                creatOrder();
-                            }}
-                        >
-                            Order Now
-                        </Button>
-                    </Box>
-                </Box>) :
+          <Box sx={{ textAlign: 'center' }} >
 
-                <Box>
-                    {
-                        WebSuccess && (
-                            <PayConfirmationModalWeb />
-                        )
-                    }
-                    {
-                        WebThanks && (
-                            <ThanksModalWeb />
-                        )
-                    }
-                    <NavBar />
-                    <Header />
-                    <AddToMealBoxWeb />
-                    <Typography style={{ fontSize: '0.8rem', fontWeight: 600, maxWidth: '75%', marginLeft: 'auto', marginRight: 'auto', marginTop: '3rem' }}>Add More Combinations</Typography>
-                    <img />
+            <Button sx={{ textTransform: 'none', backgroundColor: '#469DB1', color: 'white', width: '90%', padding: '0.75rem 3.75rem', fontSize: '1.3rem', '&:hover': { backgroundColor: '#0A343D' }, borderRadius: '2rem', marginTop: '4vh', marginBottom: '1rem' }}
+            onClick={() => {
+                setSuccess(true);
+                creatOrder();
+            }} > Order Now
+            </Button>
+
+          </Box>
+
+        </Box> 
+        ) 
+        :
+        <Box>
+          {
+            WebSuccess && (
+              <PayConfirmationModalWeb />
+            )
+          }
+          {
+            WebThanks && (
+              <ThanksModalWeb />
+            )
+          }
+          <NavBar />
+          <Header />
+          <AddToMealBoxWeb />
+          <Typography style={{ fontSize: '0.8rem', fontWeight: 600, maxWidth: '75%', marginLeft: 'auto', marginRight: 'auto', marginTop: '3rem' }}>Add More Combinations</Typography>
+          <img />
 
           <FoodAddToCart />
 
-                    <Box
-                        sx={{
-                            display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'center', marginTop: '2rem', marginBottom: '2rem'
-                        }}
-                    >
-                        <Box
-                            sx={{
-                                display: 'flex', alignItems: 'center', gap: '10px', border: '1px solid #dddd', width: '50%', marginLeft: 'auto', marginRight: 'auto', justifyContent: 'space-between', borderRadius: '3rem', paddingLeft: '3rem', boxShadow: '0px 0px 1rem #dddd'
-                            }}
-                        >
-                            <Box style={{ display: 'flex', alignItems: 'center', gap: '10px' }}>
-                                <Typography
-                                    style={{ fontSize: '1.2rem', color: '#525252', fontWeight: 500 }}
-                                >
-                                    Total Bill :
-                                </Typography>
-                                <Typography
-                                    style={{ fontSize: '1.2rem', color: '#000000', fontWeight: 600 }}
-                                >
-                                    ₹ 70
-                                </Typography>
-                            </Box>
-                            <Box style={{ display: 'flex', alignItems: 'center' }}>
-                                <Button
-                                    onClick={() => {
-                                        setWebSuccess(true);
-                                        window.scrollTo({
-                                            top: 0,
-                                            behavior: 'smooth',
-                                        });
-                                    }}
-                                    sx={{
-                                        textTransform: 'none', backgroundColor: '#469DB1', color: 'white', padding: '0.75rem 6.5rem', fontSize: '1.3rem',
-                                        '&:hover': {
-                                            backgroundColor: '#0A343D',
-                                        }, borderRadius: '2rem', width: '100%',
-                                    }}
-                                >
-                                    Order Now
-                                </Button>
-                            </Box>
-                        </Box>
-                    </Box>
+          <Box
+            sx={{
+              display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'center', marginTop: '2rem', marginBottom: '2rem'
+            }}
+          >
+            <Box
+              sx={{
+                display: 'flex', alignItems: 'center', gap: '10px', border: '1px solid #dddd', width: '50%', marginLeft: 'auto', marginRight: 'auto', justifyContent: 'space-between', borderRadius: '3rem', paddingLeft: '3rem', boxShadow: '0px 0px 1rem #dddd'
+              }}
+            >
+              <Box style={{ display: 'flex', alignItems: 'center', gap: '10px' }}>
+                <Typography
+                  style={{ fontSize: '1.2rem', color: '#525252', fontWeight: 500 }}
+                >
+                  Total Bill :
+                </Typography>
+                <Typography
+                  style={{ fontSize: '1.2rem', color: '#000000', fontWeight: 600 }}
+                >
+                  ₹ 70
+                </Typography>
+              </Box>
+              <Box style={{ display: 'flex', alignItems: 'center' }}>
+                <Button
+                  onClick={() => {
+                    setWebSuccess(true);
+                    window.scrollTo({
+                      top: 0,
+                      behavior: 'smooth',
+                    });
+                  }}
+                  sx={{
+                    textTransform: 'none', backgroundColor: '#469DB1', color: 'white', padding: '0.75rem 6.5rem', fontSize: '1.3rem',
+                    '&:hover': {
+                      backgroundColor: '#0A343D',
+                    }, borderRadius: '2rem', width: '100%',
+                  }}
+                >
+                  Order Now
+                </Button>
+              </Box>
+            </Box>
+          </Box>
 
 
-                </Box>
-            }
+        </Box>
+      }
 
-        </React.Fragment>
-    )
+    </React.Fragment>
+  )
 }
 
 export default AddTocard;
